d18e3a67a062f647e4e239bdf4a13e59bb2cfda9
Dmitriii-K <dmitriii.kirilenko@yandex.by>
2024-07-23 22:43:54 +0300

    ref tests

--- __tests__/e2e/user.e2e.test.ts
+++ __tests__/e2e/user.e2e.test.ts
@@ -12,6 +12,7 @@ import { user1 } from "./datasets";
 
 describe('E2E Tests', () => {
     const user = user1;
+    let refreshToken;
     // const session: DeviceViewModel = [];
 
     beforeAll(async () => {
@@ -36,11 +37,8 @@ describe('E2E Tests', () => {
         .set({ Authorization: "Basic " + codedAuth })
         .send(newUser) // отправка данных
         .expect(201);
-
       // console.log(res.body)
-
     expect(res.body.login).toEqual(newUser.login);
-    expect(res.body.password).toEqual(newUser.password);
     expect(res.body.email).toEqual(newUser.email);
     expect(typeof res.body.id).toEqual("string");
     });
@@ -64,15 +62,21 @@ describe('E2E Tests', () => {
             password: "password",
         });
         expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('refreshToken');
-        const refreshToken = res.body.refreshToken;
-        // global.refreshToken = res.body.refreshToken;
-        SETTINGS.REFRESH_TOKEN = refreshToken; // Достаем и сохраняем refresh token
-        console.log(refreshToken); // проверяем токен
+        expect(res.headers['set-cookie']).toBeDefined();
+        const cookies = res.headers['set-cookie'];
+        let refreshToken = null;
+        cookies.forEach(cookie => {
+            const [name, value] = cookie.split(';')[0].split('=');
+            if (name === 'refreshToken') {
+                refreshToken = value;
+            }
+        })
+        expect(refreshToken).toBeDefined();
+        console.log('Refresh Token:', refreshToken);
     });
 
     it('should use the saved refresh token', () => {
-        const refreshToken = SETTINGS.REFRESH_TOKEN;
+        // const refreshToken = SETTINGS.REFRESH_TOKEN;
         expect(refreshToken).toBeDefined();
         console.log('Using Refresh Token:', refreshToken);
         // Ваш код, использующий refreshToken
@@ -80,12 +84,12 @@ describe('E2E Tests', () => {
     it('should get all sessions', async () => {
         const res = await req
         .get(SETTINGS.PATH.SECURITY +"/devices")
-        .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+        .set('Authorization', `Bearer ${refreshToken}`);
         // .set('Authorization', `Bearer ${global.refreshToken}`);
         expect(res.statusCode).toEqual(200);
         console.log(res.body); // Логируем ответ при запросе сессий
     });
-    it.skip('Login user with different user-agents', async () => {
+    it('Login user with different user-agents', async () => {
         // for (let i = 0; i < 4; i++) {
         //     const res = await req
         //         .post(SETTINGS.PATH.AUTH +'/login')
@@ -109,11 +113,11 @@ describe('E2E Tests', () => {
     
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty('accessToken');
-        expect(res.body).toHaveProperty('refreshToken');
+        expect(res.headers['set-cookie']).toHaveProperty('refreshToken');
         }
     });
 
-    it.skip("shouldn't create 401", async () => {
+    it("shouldn't create 401", async () => {
     const newUser: UserInputModel = {
         login: "login",
         password: "password",
@@ -130,11 +134,11 @@ describe('E2E Tests', () => {
     it.skip("shouldn't create 403", async () => { // если пытаться удалить deviceId другого пользователя
         const res = await req
         .delete(SETTINGS.PATH.SECURITY +"/devices/1")
-        .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+        .set('Authorization', `Bearer ${refreshToken}`);
         expect(res.status).toBe(403);
         });
 
-    it.skip("shouldn't create 404", async () => {
+    it("shouldn't create 404", async () => {
     const res = await req
         .delete(SETTINGS.PATH.USERS +"/1")
         expect(res.status).toBe(404);
@@ -144,59 +148,54 @@ describe('E2E Tests', () => {
     it.skip('Update refreshToken for device 1', async () => {
         const res = await req
         .post(SETTINGS.PATH.AUTH +"/refresh-token")
-        .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+        .set('Authorization', `Bearer ${refreshToken}`);
         expect(res.status).toBe(200);
-        expect(res.body.refreshToken).toBe(SETTINGS.REFRESH_TOKEN);
+        expect(res.headers['set-cookie']).toBe(refreshToken);
     });
 
     it.skip('should get the list of devices with the updated refreshToken', async () => {
         const res = await req
         .get(SETTINGS.PATH.SECURITY +"/devices")
-        .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+        .set('Authorization', `Bearer ${refreshToken}`);
         expect(res.status).toBe(200);
-        expect(res.body[0].deviceId).toBe(session.devices[0].deviceId);
-        expect(res.body[0].lastActiveDate).not.toBe(session.devices[0].lastActiveDate);
     });
 
     it.skip('should delete device 2', async () => {
         const res = await req
         .delete(SETTINGS.PATH.SECURITY +`/devices/${session.devices[1].deviceId}`)
-        .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+        .set('Authorization', `Bearer ${refreshToken}`);
         expect(res.status).toBe(204);
     
         const devicesResponse = await req
         .get(SETTINGS.PATH.SECURITY +"/devices")
-        .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+        .set('Authorization', `Bearer ${refreshToken}`);
         expect(devicesResponse.status).toBe(200);
-        expect(devicesResponse.body.every((session) => session.deviceId !== session.devices[1].deviceId)).toBe(true);
     });
 
     it.skip('should logout device 3', async () => {
         const res = await req
         .post(SETTINGS.PATH.AUTH +"/logout")
-        .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+        .set('Authorization', `Bearer ${refreshToken}`);
         expect(res.status).toBe(204);
     
         const devicesResponse = await req
         .get(SETTINGS.PATH.SECURITY +"/devices")
-        .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+        .set('Authorization', `Bearer ${refreshToken}`);
         expect(devicesResponse.status).toBe(200);
-        expect(devicesResponse.body.every((session) => session.deviceId !== session.devices[2].deviceId)).toBe(true);
     });
 
     it.skip('should delete all remaining devices', async () => {
 
             const res = await req
                 .delete(SETTINGS.PATH.SECURITY +`/devices/${session.deviceId}`)
-                .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+                .set('Authorization', `Bearer ${refreshToken}`);
             expect(res.status).toBe(204);
             
     
         const devicesResponse = await req
         .get(SETTINGS.PATH.SECURITY +"/devices")
-        .set('Authorization', `Bearer ${SETTINGS.REFRESH_TOKEN}`);
+        .set('Authorization', `Bearer ${refreshToken}`);
         expect(devicesResponse.status).toBe(200);
         expect(devicesResponse.body.length).toBe(1);
-        expect(devicesResponse.body[0].deviceId).toBe(session.devices[0].deviceId);
     });
 });
