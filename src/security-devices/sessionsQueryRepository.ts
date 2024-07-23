000df964c370e3b9fe9e375af334088131ab6105
Dmitriii-K <dmitriii.kirilenko@yandex.by>
2024-07-23 23:32:11 +0300

    ref session

--- src/security-devices/sessionsQueryRepository.ts
+++ src/security-devices/sessionsQueryRepository.ts
@@ -1,3 +1,4 @@
+import { WithId } from "mongodb";
 import {sessionsCollection} from "../db/mongo-db";
 import { DeviceViewModel } from "../input-output-types/device-type";
 import { SessionsType } from "../input-output-types/sessions-types";
@@ -5,12 +6,14 @@ import { SessionsType } from "../input-output-types/sessions-types";
 export class GetAllSessions {
     static async findSessions (userId: string) {
         const currentTime = new Date();
-        const sessions: SessionsType[] = await sessionsCollection.find({user_id: userId, exp: {$gte: currentTime}}).toArray();
-        const allSessions = sessions.map(GetAllSessions.mapSession)
-        if(allSessions) return allSessions;
+        const sessions = await sessionsCollection.find({user_id: userId, exp: {$gte: currentTime}}).toArray();
+        // const allSessions = sessions.map(GetAllSessions.mapSession)
+        if(!sessions) {
             return null;
+        }
+        return GetAllSessions.mapSession(sessions)
     }
-    static mapSession (session: SessionsType): DeviceViewModel {
+    static mapSession (session: WithId<SessionsType>): DeviceViewModel {
         return {
             ip: session.ip,
             title: session.device_name,
